41ef2672cff04a61347efb25b6e473e0a89053aa
pullum327 <91268908+pullum327@users.noreply.github.com>
2021-09-23 21:55:42 +0800

    Add files via upload

---
+++ Github.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main( void ){
+    printf("%s\n", "My name is pullum");
+    printf("%s\n", "My ID is D1031052");
+    printf("%s\n", "My major is Information Engineering class A");
+}
